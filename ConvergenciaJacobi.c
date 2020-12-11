79a14bd9283f5ae41fbe3a05d68280279eef8c34
Javier Ernesto <javierernesto2000@gmail.com>
2020-12-11 10:47:03 -0300

    Resposta questao c

--- ConvergenciaJacobi.c
+++ ConvergenciaJacobi.c
@@ -84,6 +84,9 @@ espaco();
     calculaIntegracaoGauss(posicoes_iguais, resultado_equacoes, matriz, solucao_final_um, vetor_x_zero, numero_interacoes);
     CriaVetorInicial(vetor_x_zero, resultado_equacoes, posicoes_iguais);
     calculaIntegracaoJacobi(posicoes_iguais, resultado_equacoes, matriz, solucao_final_um, vetor_x_zero, numero_interacoes);
+    espaco();
+  printf("Resposta questao (C):Existe Diferenca entre os dois metodos\npois o Gauss usa os valores atuais e os anteriores e o\n Jacobi usa só os anteriores da interação atual ");
+  espaco();
 }
 
 int convergenciaJacobiDiagonalLinhas(int compara_linha[4], int compara_coluna[4], float matriz_geral[4][4])
